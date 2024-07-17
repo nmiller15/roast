60fdef5cd079e848beffa04dfd43c6a390c590ca
Nolan Miller <156489804+nmiller15@users.noreply.github.com>
2024-07-17 17:54:52 -0400

    add(Card.js)
    static card component, stores state for the favorite button

--- frontend/src/Account/Account.js
+++ frontend/src/Account/Account.js
@@ -1,11 +1,33 @@
 import React from 'react'
 import './Account.css'
+import { Settings, ProfileCircle } from 'iconoir-react'
+import Card from '../components/Card';
+
+const roastSample = {
+  name: "Ethiopian Yirgacheffe",
+  dateRoasted: new Date('July 22, 2024'),
+  percentLoss: 15.6,
+  isFavorite: true
+}
 
 function Account() {
   return (
-    <div className="Accout">
-      Account
+    <div className="Account">
+      <div className="title-bar">
+        <h1>Account</h1>
+        <Settings />
+      </div>
+      <div className="account-info">
+        <ProfileCircle />
+        <h2>Nolan Miller</h2>
+        <p className="subtitle">mail@nolanmiller.me</p>
+      </div>
+      <div className="roasts">
+        <h2>Favorite Roasts</h2>
+        <Card roast={roastSample} />
+      </div>
     </div>
+
   )
 }
 
---
+++ frontend/src/components/Card.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import './Card.css';
+import { Heart, HeartSolid } from 'iconoir-react';
+import { useState, useEffect } from 'react';
+
+function Card({ roast }) {
+    const [isFavorite, setIsFavorite] = useState(roast.isFavorite);
+    const { name, dateRoasted, percentLoss } = roast;
+    const month = dateRoasted.getMonth()
+    const day = dateRoasted.getDate()
+    const year = dateRoasted.getYear();
+
+    // Change the state of isFavorite
+    const handleHeartClick = (e) => {
+        setIsFavorite(!isFavorite);
+        //TODO: Add a logic function to add this card to the favorites list.
+    }
+
+    return (
+        <div className="Card">
+            <div className="roast-details">
+                <h3>{name}</h3>
+                <p>{month + 1}/{day}/{year} - Medium ({percentLoss}%)</p>
+            </div>
+            <div className="favorite-container" onClick={handleHeartClick}>
+                { isFavorite 
+                   ? <HeartSolid/>
+                   : <Heart /> 
+                }
+            </div>
+        </div>
+    )
+}
+
+export default Card
