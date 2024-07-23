77d14e09b36939965170f4210c7d2573046270b6
Nolan Miller <156489804+nmiller15@users.noreply.github.com>
2024-07-22 22:05:27 -0400

    add(timeString)
    format argument to get different string

--- frontend/src/util/timeString.js
+++ frontend/src/util/timeString.js
@@ -1,9 +1,15 @@
 // Seconds is an integer
-const timeString = (totalSeconds) => {
+const timeString = (totalSeconds, format = null) => {
     const minutes = Math.floor(totalSeconds / 60);
     const seconds = totalSeconds % 60;
 
-    return `${minutes ? minutes + "'" : ''}${seconds < 10 ? '0' + seconds : seconds}"`;
+    if (format === null) {
+        return `${minutes ? minutes + "'" : ''}${seconds < 10 ? '0' + seconds : seconds}"`;
+    }
+
+    if (format === 'colon') {
+        return `${minutes ? minutes + ':' : '0:'}${seconds < 10 ? '0' + seconds : seconds}`;
+    }
 }
 
 export default timeString;
