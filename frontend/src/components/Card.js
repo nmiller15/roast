1b97f94adba47d62d5246ca4252d85a683a8e495
Nolan Miller <156489804+nmiller15@users.noreply.github.com>
2024-07-24 17:07:22 -0400

    ref(Card)
    remove unused imports

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,48 +1,19 @@
 import React from 'react'
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { Heart, HeartSolid, PlusCircle } from 'iconoir-react';
 import dateFormat, { masks } from "dateformat";
 import Button from './Button';
-import Rating from './Rating';
 import roastCalc from '../util/roastCalc';
-import roastString from '../util/roastString';
 import percentLossCalc from '../util/percentLossCalc';
-import timeString from '../util/timeString';
-import gToOz from '../util/gToOz';
 import './Card.css';
 import RoastDetails from './RoastDetails';
 
 function Card({ roast }) {
     const [isActive, setIsActive] = useState(false);
     const [isFavorite, setIsFavorite] = useState(roast.isFavorite);
-    const {
-        id,
-        dateRoasted,
-        rating,
-        origin,
-        variety,
-        name,
-        startingWeightG,
-        endingWeightG,
-        heatLevel,
-        startTempF,
-        lowestTempF,
-        firstCrackSeconds,
-        tempRiseSeconds,
-        openedLidSeconds,
-        heatOffSeconds,
-        dumpedSeconds,
-        notes
-    } = roast;
+    const { dateRoasted, name, notes} = roast;
 
     const percentLoss = percentLossCalc(roast);
-    const firstCrack = timeString(firstCrackSeconds);
-    const tempRise = timeString(tempRiseSeconds)
-    const openedLid = timeString(openedLidSeconds);
-    const heatOff = timeString(heatOffSeconds);
-    const dumped = timeString(dumpedSeconds);
-    const startingOz = gToOz(startingWeightG);
-    const roastedOz = gToOz(endingWeightG);
 
     // Change the state of isFavorite
     const handleHeartClick = (e) => {
@@ -52,12 +23,10 @@ function Card({ roast }) {
 
     // Change the active state of a card
     const expand = (e) => {
-        console.log('Expanding')
         setIsActive(true);
     }
 
     const collapse = (e) => {
-        console.log('Collapsing');
         setIsActive(false);
     }
 
