6db549b6a78a43aa4c748f234c551ea5978b912d
Nolan Miller <156489804+nmiller15@users.noreply.github.com>
2024-07-31 17:19:20 +0000

    add(updateStatement)
    create update statement query from object table name and id

---
+++ backend/utils/updateStatement.js
@@ -0,0 +1,19 @@
+/*
+UPDATE roasts
+SET key = value, key1 = value1 ...
+WHERE roast_id = 3
+*/
+import { snakeCase } from "change-case";
+
+export default function updateStatement(obj, table, id) {
+  let keys = Object.keys(obj)
+  let values = Object.values(obj);
+  let statement = 'UPDATE ' + table + ' SET '
+
+  keys.forEach((key, index) => {
+    statement = statement + snakeCase(key) + ' = ' + values[index] + ' '; 
+  })
+
+  statement = statement + 'WHERE ' + table == 'users' ? 'user_id ' : table == 'roasts' ? 'roast_id ' : '' + ' = ' + id + ';'
+  return statement;
+}
